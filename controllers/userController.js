0b84de3e381ffc61db096e357e1272776341af93
Jason Kim <jason.s.kim95@gmail.com>
2022-07-11 16:34:25 -0400

    Validate and sanitize register form fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,81 @@
 const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
 exports.register_get = function (req, res, next) {
-  res.render("register", { title: "Register a New Account" });
+  res.render("register", { title: "Sign Up" });
 };
 
-exports.register_post = function (req, res, next) {
-  User.findOne({ username: req.body.username }).exec(function (err, user) {
-    if (err) return next(err);
-    if (user) {
+exports.register_post = [
+  // validate and sanitize fields
+  body("firstName", "Please enter your first name.")
+    .trim()
+    .isLength({ min: 1 })
+    .isAlpha()
+    .withMessage("First name cannot contain numbers or special characters.")
+    .escape(),
+  body("lastName", "Please enter your last name.")
+    .trim()
+    .isAlpha()
+    .withMessage("Last name cannot contain numbers or special characters.")
+    .isLength({ min: 1 })
+    .escape(),
+  body("username", "Please enter a valid username.")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Username must be at least 8 characters.")
+    .isAlphanumeric()
+    .withMessage("Username cannot contain special characters.")
+    .escape(),
+  body("password", "Please enter a valid password.")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters.")
+    .not()
+    .isUppercase()
+    .withMessage("Password must contain at least one lowercase character.")
+    .not()
+    .isLowercase()
+    .withMessage("Password must contain at least one uppercase character.")
+    .escape(),
+  // proceed to process request after validation and sanitization
+  (req, res, next) => {
+    // extract validation errors from a request
+    const errors = validationResult(req);
+
+    // create new User with escaped and trimmed data
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    if (!errors.isEmpty()) {
+      console.log(errors);
+
+      // there are errors - render form again with sanitized values & error messages
       res.render("register", {
-        title: "Register a New User",
-        errors: "Username already exists.",
+        title: "Sign Up",
+        user: newUser,
+        errors: errors.array(),
       });
     } else {
-      const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        password: req.body.password,
-      });
-
-      newUser.save(function (err) {
+      // data is valid
+      // check if username already exists
+      User.findOne({ username: req.body.username }).exec(function (err, user) {
         if (err) return next(err);
-        res.redirect("/login");
+        if (user) {
+          res.render("register", {
+            title: "Sign Up",
+            errors: "Username already exists.",
+          });
+        } else {
+          newUser.save(function (err) {
+            if (err) return next(err);
+            res.redirect("/login");
+          });
+        }
       });
     }
-  });
-};
+  },
+];
