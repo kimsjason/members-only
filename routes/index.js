13c853f923f15af4d336286f0f70e0e93b3e3d4e
Jason Kim <jason.s.kim95@gmail.com>
2022-07-11 15:36:24 -0400

    Add routes to register new users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controllers/userController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+/* GET register page. */
+router.get("/register", userController.register_get);
+
+/* POST register page. */
+router.post("/register", userController.register_post);
 
 module.exports = router;
