e5f2e5928d2dba4cb6b6fb467d3689f17fb2aede
Jason Kim <jason.s.kim95@gmail.com>
2022-07-12 22:10:39 -0400

    Add passport middleware with bcrypt password validation

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
@@ -8,6 +11,8 @@ require("dotenv").config();
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const mongoose = require("mongoose");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
 
 // mongodb connection
 mongoose.connect(process.env.DB_STRING, {
@@ -22,6 +27,48 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) {
+          return next(err);
+        }
+        if (res) {
+          // passwords match
+          return done(null, user);
+        } else {
+          // passwords do not match
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// passport middleware
+app.use(session({ secret: "members", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger("dev"));
 app.use(express.json());
