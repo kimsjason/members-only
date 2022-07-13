614fbda6b57c59bfcbec399d964ce712c45dd9ba
Jason Kim <jason.s.kim95@gmail.com>
2022-07-13 14:40:48 -0400

    Add routes for new posts

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/userController");
-const membershipController = require("../controllers/membershipController");
 const loginController = require("../controllers/loginController");
+const membershipController = require("../controllers/membershipController");
+const postController = require("../controllers/postController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -29,5 +30,11 @@ router.get("/membership", membershipController.membership_get);
 
 /* POST membership page */
 router.post("/membership", membershipController.membership_post);
+
+/* GET new post page */
+router.get("/post", postController.post_get);
+
+/* POST new post page */
+router.post("/post", postController.post_post);
 
 module.exports = router;
