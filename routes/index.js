35bd42f5907b56cb4ff952309a1d3df40624e09a
Jason Kim <jason.s.kim95@gmail.com>
2022-07-12 22:08:08 -0400

    Add login/out routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/userController");
+const loginController = require("../controllers/loginController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Members Only", user: req.user });
 });
 
 /* GET register page. */
@@ -12,5 +13,14 @@ router.get("/register", userController.register_get);
 
 /* POST register page. */
 router.post("/register", userController.register_post);
+
+/* GET login page. */
+router.get("/login", loginController.login_get);
+
+/* POST login page. */
+router.post("/login", loginController.login_post);
+
+/* GET logout */
+router.get("/log-out", loginController.logout_get);
 
 module.exports = router;
